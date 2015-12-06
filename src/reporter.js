7b25b2862740afe60a167036f22d184d056a32c6
Cameron Cundiff <github@ckundo.com>
2015-12-06 17:08:08 -0500

    Add descriptions for error message

--- src/reporter.js
+++ src/reporter.js
@@ -3,16 +3,24 @@ import request from "browser-request";
 const url = "https://accesslint-service-staging.herokuapp.com/api/v1/reports";
 
 export default function (message) {
-  let violations = message.violations;
+  const violations = message.violations;
 
   if (violations.length > 0) {
+    let descriptions = [];
+
+    violations.map((violation) => {
+      descriptions.push(violation.help);
+    });
+
     request({
       method: "POST",
       url: url,
       json: message,
     }, function() {});
 
-    console.error(message);
-    throw new Error(`AccessLintError: ${violations.length} violations.`);
+    throw new Error(
+      `AccessLintError: ${violations.length} violations:` + "\n" +
+      descriptions.join(",\n")
+    );
   }
 }
