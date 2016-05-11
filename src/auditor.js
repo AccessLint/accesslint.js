c32292f259ff7b147eb91a516dcfbed7367b2321
Cameron Cundiff <cameron@thoughtbot.com>
2016-05-11 14:54:02 -0400

    Remove scroll configuration
    - The option is not yet supported in the current release of axe-core in
      npm.
    - It was causing console errors too.

--- src/auditor.js
+++ src/auditor.js
@@ -2,13 +2,6 @@ import { axe } from "axe-core/axe.min.js";
 import report from "./reporter";
 
 export default function () {
-  window.axe.configure({
-    checks: [{
-      id: "color-contrast",
-      options: { noScroll: true }
-    }]
-  });
-
   window.axe.a11yCheck(document, (results) => {
     report(results);
   });
