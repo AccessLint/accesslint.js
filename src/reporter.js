712bb8b09caa20e5cb64e60d16894aa42a91f9c1
Cameron Cundiff <cameron@thoughtbot.com>
2016-05-11 10:59:43 -0400

    Filter fields that get POSTed
    - AccessLint::Rails complains on `null` attributes.
    - This change includes only the attributes we care about right now.

--- src/reporter.js
+++ src/reporter.js
@@ -3,13 +3,32 @@ import request from "browser-request";
 const url = "/access_lint/errors"
 
 export default function (message) {
-  const violations = message.violations;
+  var violations = message.violations.map(function(violation) {
+    return {
+      description: violation.description,
+      help: violation.help,
+      helpUrl: violation.helpUrl,
+      id: violation.id,
+      nodes: violation.nodes.map(function(n) {
+        return {
+          target: n.target,
+          impact: n.impact
+        };
+      }),
+      tags: violation.tags
+    };
+  });
 
   if (violations.length > 0) {
     request({
       method: "POST",
       url: url,
-      json: message,
+      json: {
+        accesslint: {
+          violations: violations,
+          url: window.location.pathname
+        }
+      }
     }, function() {});
   }
 }
